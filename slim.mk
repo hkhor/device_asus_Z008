8f2d1b4aadb76f30b6631254787635e3e3cf412a
deadman96385 <seanhoyt963@gmail.com>
2015-08-05 02:12:14 -0400

    Slimify
    Change-Id: I5b602d12ef31ad33fc5ecf5b6eb4a626506f4fec

--- slim.mk
+++ slim.mk
@@ -2,10 +2,10 @@
 TARGET_SCREEN_HEIGHT := 720
 TARGET_SCREEN_WIDTH := 1280
 
-# Inherit some common CM stuff.
+# Inherit some common Slim stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z008/device.mk)
@@ -15,7 +15,7 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/Z008/overlay
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z008
+PRODUCT_NAME := slim_Z008
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z008
 PRODUCT_MANUFACTURER := asus
